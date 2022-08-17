4ef53e49d95e4693500461de77ca222b089cd365
Pacifist <desmondmorales92@gmail.com>
2022-08-17 17:17:31 +0300

    design

--- scripts/view.js
+++ scripts/view.js
@@ -18,16 +18,16 @@ export class View {
     renderContact (data) {
         let contactContainer = document.querySelector(`#contact-container`)
         let wrapper = document.createElement(`div`)
-        wrapper.classList.add(`contact`, `mt-3`)
+        wrapper.classList.add(`contact`)
         wrapper.setAttribute(`data-id`, `${data.id}`)
         wrapper.innerHTML=`
         <p class="contact_name">Name: ${data.name}</p>
         <p class="contact_number">Phone: ${data.phone}</p>
         <p class="contact_position">Position: ${data.position}</p>
         <div class="contact_options">
-            <button class="contact_remove">Remove</button>
-            <button class="contact_change">Change</button>
-            <button class="contact_info">Info</button>
+            <button class="contact_remove btn btn-danger">Remove</button>
+            <button class="contact_change btn btn-primary">Change</button>
+            <button class="contact_info btn btn-secondary">Info</button>
         </div>
         `
         contactContainer.append(wrapper)
@@ -40,7 +40,7 @@ export class View {
         wrapper.classList.add(`contact-container`, `mt-3`)
         wrapper.setAttribute(`id`, `contact-container`)
         wrapper.innerHTML=`
-        <p>Your contacts:</p>
+        <p class="container_par">Your contacts:</p>
         `
         formWrapper.append(wrapper)
     }
@@ -51,7 +51,7 @@ export class View {
         wrapper.classList.add(`no-contacts`, `mt-3`)
         wrapper.setAttribute(`id`, `no-contacts`)
         wrapper.innerHTML=`
-        <p>No contacts</p>
+        <p class="no_contacts_par">No contacts</p>
         `
         formWrapper.append(wrapper)
     }
@@ -73,22 +73,26 @@ export class View {
     }
 
     changeContact (form, data) {
-        // document.querySelector("#" + `${form}`).innerHTML = ``
-        // document.querySelector(`#form-wrapper`).innerHTML = ``
         const wrapper = document.createElement(`form`)
         wrapper.classList.add(`formChange`)
         wrapper.setAttribute(`id`, `formChange`)
         wrapper.setAttribute(`data-id`, `${data.id}`)
         wrapper.innerHTML=`
-        <label for="contact-name" class="col-form-label">Name: </label>
-        <input type="text" name="name" class="contact_name" id="contact-name" placeholder=${data.name}></input>
-        <label for="contact-phone" class="col-form-label">Phone: </label>
-        <input type="tel" name="phone" class="contact_number" id="contact-phone" placeholder=${data.phone}></input>
-        <label for="contact-position" class="col-form-label">Name: </label>
-        <input type="text" name="position" class="contact_position" id="contact-position" placeholder=${data.position}></input>
+        <div class="change_name_block">
+            <label for="contact-name" class="col-form-label">Name: </label>
+            <input type="text" name="name" class="contact_name" id="contact-name" placeholder=${data.name}></input>
+        </div>
+        <div class="change_phone_block">
+            <label for="contact-phone" class="col-form-label">Phone: </label>
+            <input type="tel" name="phone" class="contact_number" id="contact-phone" placeholder=${data.phone}></input>
+        </div>
+        <div class="change_position_block">
+            <label for="contact-position" class="col-form-label">Name: </label>
+            <input type="text" name="position" class="contact_position" id="contact-position" placeholder=${data.position}></input>
+        </div>
         <div class="contact_options">
-            <button class="contact_close">Close</button>
-            <button type="submit" class="contact_save">Save changes</button>
+            <button class="contact_close btn btn-dark">Close</button>
+            <button type="submit" class="contact_save btn btn-success">Save changes</button>
         </div>
         `
 
@@ -98,14 +102,14 @@ export class View {
 
     renderInfo (data) {
         let wrapper = document.createElement(`div`)
-        wrapper.classList.add(`contact`, `mt-3`)
+        wrapper.classList.add(`contact_info_page`, `mt-3`)
         wrapper.setAttribute(`data-id`, `${data.id}`)
         wrapper.innerHTML=`
         <p class="contact_name" >Name: ${data.name}</p>
         <p class="contact_number">Phone: ${data.phone}</p>
         <p class="contact_position">Position: ${data.position}</p>
         <div class="contact_options">
-            <button class="contact_close" id="contact_close">Close</button>
+            <button class="contact_close btn btn-dark" id="contact_close">Close</button>
         </div>
         `
 
